b6e1c730d1eb08bd2463cf7a9f56c960fb6a4a79
hendisantika <hendisantika@yahoo.co.id>
2019-01-22 06:52:49 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/simpleauthjwtservice/SimpleAuthJwtServiceApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.simpleauthjwtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAuthJwtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAuthJwtServiceApplication.class, args);
+    }
+
+}
+
